4e5bd09fbccd61bd6befaf7b7f24649104a8f384
jreeves3 <jereeves@andrew.cmu.edu>
2025-08-07 14:05:41 -0400

    updated run script to match alignment and ulc reencodin

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-./cadical/build/cadical $1 $2/proof.out --ulc  --ulcype=3 --no-binary 
+./cadical/build/cadical $1 $2/proof.out --no-binary --ulc=1 --ulctype=1 --ulcaligntype=1 --ulcalign=1 --ulcelim=0
